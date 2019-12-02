a49e2e41d1bd3f9cfb8e5ecafa4b17038cf9c1d6
Ignas Maslinskas <5737899+Hoffs@users.noreply.github.com>
2019-12-02 12:59:14 +0200

    Finished solution
    Simple bruteforce

--- day2/solution.py
+++ day2/solution.py
@@ -1,5 +1,23 @@
 
-isTest = True
+def runCalculations(tInput):
+    currPos = 0
+
+    while tInput[currPos] != '99':
+        fromA = int(tInput[int(tInput[currPos+1])])
+        fromB = int(tInput[int(tInput[currPos+2])])
+        to = int(tInput[currPos+3])
+        # print(fromA, fromB, to)
+        if (tInput[currPos] == '1'):
+            result = fromA + fromB
+            tInput[to] = result
+        elif (tInput[currPos] == '2'):
+            result = fromA * fromB
+            tInput[to] = result
+        currPos += 4
+        # print(tInput)
+    return tInput[0]
+
+isTest = False
 inputFile = 'input.txt'
 if (isTest):
     inputFile = 'test.txt'
@@ -9,20 +27,16 @@ inputLine = ''
 for line in open(inputFile, 'r'):
     inputLine = line
 
-inputSplit = inputLine.split(',')
+originalInput = inputLine.split(',')
 
-currPos = 0
-
-while inputSplit[currPos] != '99':
-    fromA = int(inputSplit[currPos+1])
-    fromB = int(inputSplit[currPos+2])
-    to = int(inputSplit[currPos+3])
-    if (inputSplit[currPos] == '1'):
-        result = fromA + fromB
-        inputSplit[to] = result
-    elif (inputSplit[currPos] == '2'):
-        result = fromA * fromB
-        inputSplit[to] = result
-    currPos += 4
-
-print(inputSplit)
+for noun in range(0, 100):
+    for verb in range(0, 100):
+        currentInput = originalInput.copy()
+        currentInput[1] = noun
+        currentInput[2] = verb
+        print(currentInput, noun, verb)
+        result = runCalculations(currentInput)
+        print(result)
+        if (result == 19690720):
+            print(noun, verb)
+            quit()          
