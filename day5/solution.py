64bbcc84060131f741c8f7f558d36116d46e3e8a
Ignas Maslinskas <5737899+Hoffs@users.noreply.github.com>
2019-12-05 13:58:29 +0200

    Solution for day 5

--- day5/solution.py
+++ day5/solution.py
@@ -1,49 +1,87 @@
 
 def getOpInfo(op):
+    op = str(op).rjust(5, '0')
     opCode = int(op[-2:])
-    modA = int(op[0])
-    modB = int(op[1])
-    return (opCode, modA, modB) 
+    modThird = int(op[0])
+    modSecond = int(op[1])
+    modFirst = int(op[2])
+    return (opCode, modFirst, modSecond, modThird) 
+
+def getValue(inArr, pos, mod):
+    if (mod == 0):
+        return int(inArr[int(inArr[pos])])
+    elif (mod == 1):
+        return int(inArr[pos])
+    else:
+        raise Exception('Bad modifier ' + mod)
 
 def runCalculations(tInput):
     currPos = 0
+    while True:
+        (opCode, modFirst, modSecond, modThird) = getOpInfo(tInput[currPos])
 
-    while tInput[currPos] != '99':
-        fromA = int(tInput[int(tInput[currPos+1])])
-        fromB = int(tInput[int(tInput[currPos+2])])
-        to = int(tInput[currPos+3])
-        # print(fromA, fromB, to)
-        
-        if (tInput[currPos] == '1'):
-            result = fromA + fromB
-            tInput[to] = result
-        elif (tInput[currPos] == '2'):
-            result = fromA * fromB
-            tInput[to] = result
-        currPos += 4
-        # print(tInput)
-    return tInput[0]
+        if opCode == 1:
+            a = getValue(tInput, currPos + 1, modFirst)
+            b = getValue(tInput, currPos + 2, modSecond)
+            c = getValue(tInput, currPos + 3, 1)
+            tInput[c] = a + b
+            currPos += 4
+        elif opCode == 2:
+            a = getValue(tInput, currPos + 1, modFirst)
+            b = getValue(tInput, currPos + 2, modSecond)
+            c = getValue(tInput, currPos + 3, 1)
+            currPos += 4
+            tInput[c] = a * b
+        elif opCode == 3:
+            inpt = int(input("OPCODE3: "))
+            c = getValue(tInput, currPos + 1, 1)
+            tInput[c] = inpt
+            currPos += 2
+        elif opCode == 4:
+            a = getValue(tInput, currPos + 1, modFirst)
+            currPos += 2
+        elif opCode == 5:
+            a = getValue(tInput, currPos + 1, modFirst)
+            b = getValue(tInput, currPos + 2, modSecond)
+            if a != 0:
+                currPos = b
+            else:
+                currPos += 3
+        elif opCode == 6:
+            a = getValue(tInput, currPos + 1, modFirst)
+            b = getValue(tInput, currPos + 2, modSecond)
+            if a == 0:
+                currPos = b
+            else:
+                currPos += 3
+        elif opCode == 7:
+            a = getValue(tInput, currPos + 1, modFirst)
+            b = getValue(tInput, currPos + 2, modSecond)
+            c = getValue(tInput, currPos + 3, 1)
+            if a < b:
+                tInput[c] = 1
+            else:
+                tInput[c] = 0
+            currPos += 4
+        elif opCode == 8:
+            a = getValue(tInput, currPos + 1, modFirst)
+            b = getValue(tInput, currPos + 2, modSecond)
+            c = getValue(tInput, currPos + 3, 1)
+            if a == b:
+                tInput[c] = 1
+            else:
+                tInput[c] = 0
+            currPos += 4
+        elif opCode == 99:
+            quit()
+        else:
+            raise Exception('Unknown opcode ' + opCode)
 
 isTest = False
 inputFile = 'input.txt'
 if (isTest):
     inputFile = 'test.txt'
 
-inputLine = ''
-
 for line in open(inputFile, 'r'):
-    inputLine = line
-
-originalInput = inputLine.split(',')
-
-for noun in range(0, 100):
-    for verb in range(0, 100):
-        currentInput = originalInput.copy()
-        currentInput[1] = noun
-        currentInput[2] = verb
-        print(currentInput, noun, verb)
-        result = runCalculations(currentInput)
-        print(result)
-        if (result == 19690720):
-            print(noun, verb)
-            quit()          
+    result = runCalculations(line.split(','))
+    print(result)
